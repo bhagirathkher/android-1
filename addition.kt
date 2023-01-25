ba7c5979e554019e1b492091257b8a79ea2e6440
bhagirathkher <123075549+bhagirathkher@users.noreply.github.com>
2023-01-25 17:05:14 +0530

    Add files via upload

---
+++ addition.kt
@@ -0,0 +1,16 @@
+fun main (
+
+){
+    var a=8
+    var b=9
+    var c=a+b
+    var d=a-b
+    var e=a/b
+    var f=a*b
+
+
+    println("addition is : $c")
+    println("substriction is : $d")
+    println("divisoin is : $e")
+    println("multiplication is $f")
+}
