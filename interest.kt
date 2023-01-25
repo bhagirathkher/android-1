31bd038e0266f38ae82db9f167b044b0759e69a1
bhagirathkher <123075549+bhagirathkher@users.noreply.github.com>
2023-01-25 17:30:15 +0530

    Add files via upload

---
+++ interest.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    print("enter your S  : ")
+    var S= readLine()!!.toFloat()
+    print("enter your M  : ")
+    var M= readLine()!!.toFloat()
+    print("enter your X  : ")
+    var X= readLine()!!.toFloat()
+    var I=S * M * X/100
+    println("simple interest is : $I ")
+}
