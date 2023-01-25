acb7c12f9a4b4dfe908ee6333df58c523d442137
bhagirathkher <123075549+bhagirathkher@users.noreply.github.com>
2023-01-25 17:05:32 +0530

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    var name="bhagirath kher"
+    var dbirth="5-aug-2004"
+    var age=19
+    var address="junagadh,mangrol" +
+
+
+
+            println("your name is $name and your birth of date $dbirth ")
+            println("your age is $age and your currunt address is $address")
+}
