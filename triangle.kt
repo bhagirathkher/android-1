78e742baba5cda3a827ac65b5ecfbffa86e1708f
bhagirathkher <123075549+bhagirathkher@users.noreply.github.com>
2023-01-25 17:22:47 +0530

    Add files via upload

---
+++ triangle.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    print("enter value of n :")
+    var n= readLine()!!.toFloat()
+    print("enter value of m :")
+    var m= readLine()!!.toFloat()
+    var area =(n*m)/2
+    println("area of triangle is :  $area")
+}
